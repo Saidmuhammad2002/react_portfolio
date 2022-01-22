305c723caab22436f6951588b828e2cea76ebade
Saidmuhammad2002 <sololearner567@gmail.com>
2022-01-23 01:19:56 +0500

    google console traning

--- router.js
+++ router.js
@@ -1,13 +1,17 @@
 import React from "react";
 import { BrowserRouter, Route, Switch } from "react-router-dom";
+import About from "./src/pages/About";
+import Contact from "./src/pages/Contact";
+import Home from "./src/pages/Home";
+import Projects from "./src/pages/Projects";
 
 export default (
   <BrowserRouter>
     <Switch>
-      <Route path="/" />
-      <Route path="/about" />
-      <Route path="/projects" />
-      <Route path="/contact" />
+      <Route path="/" component={Home} />
+      <Route path="/about" component={About} />
+      <Route path="/projects" component={Projects} />
+      <Route path="/contact" component={Contact} />
       <Route />
     </Switch>
   </BrowserRouter>
