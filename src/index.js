692a10bb20b6c6bf33abf7ed77139e62b88e4f98
Saidmuhammad2002 <sololearner567@gmail.com>
2022-01-23 01:15:21 +0500

    google console traning

--- src/index.js
+++ src/index.js
@@ -3,13 +3,14 @@ import ReactDOM from "react-dom";
 import App from "./App";
 import GlobalStyles from "./styles/GlobalStyle";
 import Typography from "./styles/Typography";
+import { Helmet } from "react-helmet";
 
 ReactDOM.render(
   <>
     <Helmet>
       <meta charSet="utf-8" />
       <title>Saidmuhammads Best Portfolio ever</title>
-      <link rel="canonical" href="https://saidmuhammadportfolio.netlify.app/")" />
+      <link rel="canonical" href="https://saidmuhammadportfolio.netlify.app/" />
     </Helmet>
     <GlobalStyles />
     <Typography />
